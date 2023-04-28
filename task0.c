13c1fdb2a266d917c4913b5bef22e99d5a7f617d
Kana Azibabari Lagalo <kanaazibabari@gmail.com>
2023-04-28 12:10:20 -0700

    Kana Lagalo

--- task0.c
+++ task0.c
@@ -1,7 +1,7 @@
+#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include "main.h"
-#include <stdio.h>
 #include <stddef.h>
 /**
  * _printf - prints a formatted string to the standard output
@@ -11,44 +11,43 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int printed_chars = 0;
-va_start(args, format);
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-char c = va_arg(args, int);
-write(1, &c, 1);
-printed_chars++;
-}
-else if (*format == 's')
-{
-char *s = va_arg(args, char *);
-while (*s)
-{
-write(1, s, 1);
-s++;
-printed_chars++;
-}
-}
-else if (*format == '%')
-{
-write(1, "%", 1);
-printed_chars++;
-}
-}
-else
-{
-write(1, format, 1);
-printed_chars++;
-}
-format++;
-}
-va_end(args);
+	va_list arg;
+	int i = 0, count = 0;
 
-return (printed_chars);
+	va_start(arg, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			switch (format[++i])
+			{
+			case 'c':
+				putchar(va_arg(arg, int));
+				count++;
+				break;
+			case 's':
+				count += puts(va_arg(arg, char *));
+				break;
+			case '%':
+				putchar('%');
+				count++;
+				break;
+			default:
+				putchar('%');
+				count++;
+				putchar(format[i]);
+				count++;
+				break;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(arg);
+
+	return (count);
 }
